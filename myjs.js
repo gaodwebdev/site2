c5082e1d17a3c6610ce3bd6db66f373ecc1b0809
Gabe <gaodwebdev@gmail.com>
2016-10-14 02:44:45 +0100

    first commit

---
+++ myjs.js
@@ -0,0 +1,29 @@
+
+
+  function doAdd(){
+  var value1=new Number(document.getElementById("Value1").value);
+  var value2=new Number(document.getElementById("Value2").value);
+  document.getElementById("Result").setAttribute("value", value1 + value2);
+  }
+  function doSub(){
+  var value1=new Number(document.getElementById("Value1").value);
+  var value2=new Number(document.getElementById("Value2").value);
+  document.getElementById("Result").setAttribute("value", value1 - value2);
+  }
+  function doMult(){
+  var value1=new Number(document.getElementById("Value1").value);
+  var value2=new Number(document.getElementById("Value2").value);
+  document.getElementById("Result").setAttribute("value", value1 * value2);
+  }
+  function doDiv(){
+  var value1=new Number(document.getElementById("Value1").value);
+  var value2=new Number(document.getElementById("Value2").value);
+  document.getElementById("Result").setAttribute("value", value1 / value2);
+  }
+function aler(){
+
+    document.write("this is the second");
+
+}
+
+
